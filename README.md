e069598b9d018f471d1c558d34383e454934c1e1
evalcony <evalcony@163.com>
2023-12-08 02:07:44 +0800

    1. 提取readme中文件内容 2. 若干fix

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # local_scanner
-对本地目录进行扫描（限定深度），对扫描目录结果进行标注
+- 对本地目录进行扫描（限定深度）。
+- 对于目录下包含 readme.md 的，视为代码项目，会自动提取 readme.md 中内容，进行标注。
+- 包含若干截止扫描条件，视为扫描终点，防止扫描非重要目录。
 
 使用方法
 ```commandline
-python3 scan.py -f '/Users/evalcony/coding' -d 3 > temp.txt
+python3 scan.py -f '/Users/evalcony/coding' -d 4 > result.txt
 ```
+
+参数介绍
+
+- `-d` 表示扫描深度，默认为3。
+- `-f` 表示扫描路径。
--- scan.py
+++ scan.py
@@ -2,7 +2,7 @@ import argparse
 import os
 
 # 当父目录包含这些子目录时，则不对父目录进行遍历
-stopfile_set = {'.github', '.git', '.githooks', '.idea', 'src', 'classes', 'test', 'resources', 'logs'}
+stopfile_set = {'.github', '.git', '.githooks', '.idea', 'src', 'classes', 'test', 'resources', 'logs', 'css', 'LICENSE', 'pom.xml'}
 # 忽略对这些目录的遍历
 ignore_set = {'zm-code', 'shareOI', 'weibo-export-test', 'weibo-export'}
 
@@ -13,28 +13,82 @@ def main(args):
         print('参数错误')
         return
     # 遍历
-    traverse(filepath, depth, '')
+    traverse(filepath, depth, '', filepath)
 
-def traverse(path, depth, indentation):
+def traverse(path, depth, indentation, base_path):
     if depth == 0:
-        return
+        return ''
 
+    # print(path)
+    readme_name = ''
     stop_flag = False
+    # 如果当前目录文件中有 readme.md，则读取相关内容
     for file in os.listdir(path):
-        if file in stopfile_set:
-            stop_flag = True
+        if file.lower() == 'readme.md':
+            readme_name = getname_by_readme(os.path.join(path, file))
             break
 
-    if stop_flag:
-        return
+    if readme_name == '':
+        for file in os.listdir(path):
+            if file in stopfile_set:
+                stop_flag = True
+                break
+
+    if stop_flag or readme_name != '':
+        print(indentation + path.replace(base_path, '') + readme_name)
+        return readme_name
+
     for file in os.listdir(path):
         if file in ignore_set:
             continue
 
         file_path = os.path.join(path, file)
         if os.path.isdir(file_path):
-            print(indentation+file_path.replace(path, ""))
-            traverse(file_path, depth - 1, indentation+'|----')
+            name = traverse(file_path, depth - 1, indentation + '|----', base_path)
+            if name == '':
+                print(indentation+file_path.replace(base_path, ''))
+    return ''
+
+def getname_by_readme(readme_file):
+    lines = []
+    with open(readme_file, 'r') as file:
+        for line in file:
+            lines.append(line.replace("\n", ""))
+    # 返回可能的文件名
+    first_ctnt = ''
+    for i in range(len(lines)):
+        line = lines[i]
+        if (line.strip() == ''
+            or line.strip().startswith('===')
+            or line.strip().startswith('[!')
+            or line.strip().startswith('![')
+            or line.strip().startswith('<html>')
+            or line.strip().startswith('<') and line.find('<h1') == -1
+            or line.find('](') != -1
+            or line.find('简介') != -1
+            or line.find('前言') != -1
+            or line.find('目录') != -1
+            or line.find('介绍') != -1
+            or line.find('技术') != -1
+            or line.find('版本') != -1
+            or line.find('项目地址') != -1
+            or line.find('项目结构') != -1):
+            lines[i] = ''
+            continue
+
+        if (first_ctnt == '' and (
+                line.strip().startswith('# ')
+                or line.find('<h1') != -1 or line.find('<h2') != -1 or line.find('<h3') != -1
+        )):
+            first_ctnt = line
+            break
+    # 如果第一遍扫描之后未找到合适的行作为title，则再遍历一遍
+    if first_ctnt == '':
+        for i in range(len(lines)):
+            if lines[i] != '':
+                first_ctnt = lines[i]
+                break
+    return first_ctnt
 
 
 if __name__ == '__main__':
